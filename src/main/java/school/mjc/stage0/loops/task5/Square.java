67b489611806515d4e743fb0dae2b59718d688fe
ZhilnikovT <126979946+ZhilnikovT@users.noreply.github.com>
2023-03-27 12:41:32 +0300

    Update Square.java

--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -1,7 +1,17 @@
 package school.mjc.stage0.loops.task5;
 
 public class Square {
-    public void printSquareFrom8s(int sideLength){
-
-    }
+     public void printSquareFrom8s(int sideLength){
+        for(int y = 1; y <= sideLength; y++){
+            for(int x = 1; x <= sideLength; x++){
+                if((y > 1 && y < sideLength) && (x > 1 && x < sideLength)) {
+                    System.out.print(" ");
+                }else{
+                    System.out.print("8");
+                }
+                
+            }
+            System.out.print("\n");
+        }
+    } 
 }
